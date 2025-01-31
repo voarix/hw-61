ef6c144d7cfde490f267c7e41575db2dc29f1104
Kachkynov Alikhan <t8t228@gmail.com>
2025-01-31 12:52:55 +0600

    сделал запрос на все страны и вывожу их в отдельном компоненте SideBar.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,28 @@
 import "./App.css";
+import axios from "axios";
+import { BASE_URL, URL_COUNTRIES } from "./globalConstants.ts";
+import { useCallback, useEffect, useState } from "react";
+import { ApiResponse } from "./types";
+import SideBar from "./components/SideBar.tsx";
 
 const App = () => {
+  const [countries, setCountries] = useState<ApiResponse[]>([]);
 
+  const fetchData = useCallback(async () => {
+    const response = await axios.get<ApiResponse[]>(BASE_URL + URL_COUNTRIES);
+    console.log(response.data);
+    setCountries(response.data);
+    return response.data;
+  }, []);
+
+  useEffect(() => {
+    void fetchData();
+  }, [fetchData]);
 
   return (
-    <div className='container'></div>
+    <div className='container vh-100 p-3'>
+      <SideBar countries={countries}/>
+    </div>
   );
 };
 
---
+++ src/types.d.ts
@@ -0,0 +1,5 @@
+export interface ApiResponse {
+  name: string;
+  alpha3Code: string;
+  independent: boolean;
+}
